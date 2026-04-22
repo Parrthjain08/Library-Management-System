cbf9a9696a556aad315fcade6e451ba41d135f18
Parrth Jain <2510001423@cgcuniversity.org>
2026-04-22 14:30:22 +0530

    Create Show books.py

---
+++ Show books.py
@@ -0,0 +1,9 @@
+# ^ SHOW BOOKS
+def show_books():
+    if len(books) == 0:
+        print("No books available")
+    else:
+        print("Books available")
+        for b in books:
+            print(b)   
+                             
